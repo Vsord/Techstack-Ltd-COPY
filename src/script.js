98a8ae90fd1c05d9bef5a078dce6d6806075e2dc
Vsord <stanisla060977@gmail.com>
2022-04-17 13:50:35 +0300

    finishing

--- src/script.js
+++ src/script.js
@@ -1,20 +1,22 @@
+//Variables/////////////////////////
 const buttonsClass = document.querySelector('.burger-line-container');
 const dropDownList = document.querySelector('.dropdown-list');
 const input = document.querySelector('.header-input');
+input.addEventListener('input', searchArea);
 const scrollToTopBtn = document.getElementById('scroll-to-top');
 const preloader = document.querySelector('.preloader-container');
+const inputSearchArea = document.getElementById('search-area');
+////////////////////////////////////
 
-// XML data request
+
+////////////////// XML data request
 const requstUrl = "https://jsonplaceholder.typicode.com/posts";
 
 function sendRequest(method, url, body = null) {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url);
-
         xhr.responseType = 'json';
-        // xhr.setRequestHeader('Content-Type','application/json')
-
         xhr.onload = () => {
             if (xhr.status >= 400) {
                 reject(xhr.response)
@@ -22,26 +24,16 @@ function sendRequest(method, url, body = null) {
                 resolve(xhr.response);
             }
         };
-
         xhr.onerror = () => {
             reject(xhr.response);
         };
-
-        // xhr.send(JSON.stringify(body));
         xhr.send();
     });
 };
-// const body = {
-//     name: 'Stanislav',
-//     age: 23
-// }
-//
-// sendRequest('GET', requstUrl, body)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+//////////////////////////////////////
 
 
-// preloader + data request
+////////////// preloader + data request
 
 function preloaderData() {
     setTimeout(() => {
@@ -53,9 +45,10 @@ function preloaderData() {
     }, 2000)
 };
 preloaderData();
+///////////////////////////////////////
 
+///////////// scroll to top button change
 
-// scroll to top button change
 window.onscroll = function () {
     ScrollToTop()
 };
@@ -69,8 +62,13 @@ function ScrollToTop() {
 };
 
 function topFunction() {
-    document.documentElement.scrollTop = 0;
+    document.getElementById('header-scroll-to')
+        .scrollIntoView({
+            behavior: 'smooth'
+        });
 };
+
+//////////////////////////////////////////////
 
 // Burger menu
 function changeBurgerLines() {
@@ -87,14 +85,37 @@ function changeInput() {
     })
 };
 
+////////////////////////////////////////////////
 
 
+// Search-input-area appearing/////////////////
 
+function searchArea() {
+    if (input.value === '') {
+        inputSearchArea.style.display = 'none';
+    } else {
+        inputSearchArea.style.display = 'block';
+    }
+};
+/////////////////////////////////////////////////
+// Search input data by integers /////////////////////
+input.oninput = function () {
+    let val = this.value.trim();
+    let searchInputItems = document.querySelectorAll('.active ul li');
 
-// function changeToTopButton() {
-//     if (window.screen.width < 301) {
-//       scrollToTopBtn.innerText = 'fff'
-//     }
-// };
-// changeToTopButton();
-
+    if (val !== '') {
+        searchInputItems.forEach(function(elem) {
+            if (elem.innerText.search(val) === -1) {
+                elem.style.display = 'none'
+            } else {
+                elem.style.display = 'block'
+            }
+        });
+    } else {
+        searchInputItems.forEach(function(elem) {
+            if (elem.innerText.search(val) === -1) {
+                elem.style.display = 'none'
+            }
+        })
+    }
+}
